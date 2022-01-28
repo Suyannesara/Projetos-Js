99fc7ed8716115abd1f66a85ed828f879d0fe403
Suyannesara <suyannesara17@gmail.com>
2022-01-28 11:21:21 -0300

    Working with %

---
+++ prj8.js
@@ -0,0 +1,22 @@
+/* RESTO DE DIVISÃO - % */
+/*
+let resto = 5 % 2;
+console.log(resto);*/
+
+function igualdade(inteiro){
+  if (inteiro % 2 == 0) {
+    return inteiro = (`O NÚMERO ${x} É PAR, BÊBÊ!`);
+  
+  }else{
+    return inteiro (`O NÚMERO ${x} É ÍMPAR, BÊBÊ!`);
+  };
+  
+}
+
+
+if (x % 2 == 0) {
+  console.log(`O NÚMERO ${x} É PAR, BÊBÊ!`);
+
+}else{
+  console.log(`O NÚMERO ${x} É ÍMPAR, BÊBÊ!`);
+};
