1f98dd6669e0a5c385fac4ccbd8c932c74b5d184
Suyannesara <suyannesara17@gmail.com>
2022-02-28 16:30:53 -0300

    Exercise 11, done

--- exLogica/n11.js
+++ exLogica/n11.js
@@ -1,5 +1,35 @@
+//Ask user for the numbers and type of operation
 
+var n1 = parseFloat(prompt("Tell me the first number: "))
+var n2 = parseFloat(prompt("Tell me the second number: "))
 
+var operation = prompt("What operation do you wanna do? (Type only: +, -, *, /")
+
+var resultado
+
+switch (operation){
+  case "+":
+    resultado = n1 + n2
+  break;
+
+  case "-":
+    resultado = n1 - n2
+  break;
+
+  case "*":
+    resultado = n1 * n2
+  break;
+  
+  case "/":
+    resultado = n1 / n2
+  break;
+
+  default:
+    alert("The operation you asked for isn't in this algorithym scope")
+  break;
+}
+
+alert("The result of your operation is: " + resultado)
 
 
 
