e31481b5a7bebddd1571df6e20fbe56f7cb44360
Suyannesara <suyannesara17@gmail.com>
2022-03-03 20:42:35 -0300

    Exercise 12 done

---
+++ exLogica/n12.js
@@ -0,0 +1,19 @@
+    /* N12 - */
+    var grade = parseInt(prompt("Tell me what was your grade? "));
+
+    //if structure
+
+    if (grade < 3){
+      alert("E")
+    } 
+    else if (grade >= 3 && grade <=5){
+      alert("D")
+    } 
+    else if (grade == 6 || grade == 7){
+      alert("C")
+    } 
+    else if (grade == 8 || grade == 9){
+      alert("B")
+    } else{
+      alert("A")
+    }
